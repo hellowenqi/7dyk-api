eb8abbad5cbcaddbc358642d15ed01f782c2571c
wuwq <wuwenqi@neotel.com.cn>
2016-08-13 21:05:37 +0800

    验证码和登录

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends HttpKernel {
 	protected $routeMiddleware = [
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'wechatauth' => 'App\Http\Middleware\Wechatauth',
-		'adminauth' => 'App\Http\Middleware\Adminauth',
+		'adminauth' => 'App\Http\Middleware\AdminAuth',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
 	];
