bf5525d12ab00b2ea3e0e0ee8f7ed52eed1a874e
wuwq <wuwenqi@neotel.com.cn>
2016-08-23 22:28:07 +0800

    delete important  file

--- config/inner.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'token' => 'u6EieSEXWExDRP3xYwELv3yP8Vgx5yzl'
-];
--- config/wechat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	//'appid' => 'wxef131d66470bd9c2',
-	//'appsecret' => '81743069477d9383e6591019ee1881ba',
-    'appid' => 'wxfbe89421611bdb2d',
-    'appsecret' => 'cd9c70575b4895cf7e810dce18bd47bd',
-	'token' => 'weixintoken',
-];
