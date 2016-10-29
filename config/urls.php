5759809c415cd8ec0a661f3b5ab1b720513959d5
57 <njwuqi123@live.com>
2016-10-29 22:38:17 +0800

    serser settion is okay

--- config/urls.php
+++ config/urls.php
@@ -4,5 +4,5 @@ return [
     'path'   => '/root/cer',
     'adminLogin' => 'http://h5app.7dyk.com/ama/admin/public/login',
     'picUrl' => 'http://img.7dyk.com/',
-    'picPath' => public_path() . DIRECTORY_SEPARATOR . 'pic',
+    'picPath' => '/var/www/html/img',
 ];
